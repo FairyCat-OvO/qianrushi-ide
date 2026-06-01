0e3d5e3cb87c3c3c3e52ef9bf46178131403e163
FairyCat <xilanyunyuan@vip.qq.com>
2026-06-01 16:58:34 +0800

    现在可以显示中文了!

--- class/auto_manual_control/display.cpp
+++ class/auto_manual_control/display.cpp
@@ -11,36 +11,46 @@ U8G2_SH1106_128X64_VCOMH0_F_HW_I2C u8g2(U8G2_R0);
 void display_init(void) {
   Wire.begin(I2C_SDA, I2C_SCL);
   u8g2.begin();
+  u8g2.enableUTF8Print();
+  u8g2.setFont(u8g2_font_wqy12_t_gb2312a);
   u8g2.setContrast(0x60);
   u8g2.setPowerSave(0);
-  u8g2.setFont(u8g2_font_ncenB08_tr);
   
-  u8g2.clearBuffer();
-  u8g2.drawFrame(0, 0, 128, 64);
-  u8g2.drawStr(10, 15, "PCT_100_CTL");
-  u8g2.drawStr(10, 35, "INIT...");
-  u8g2.sendBuffer();
+  u8g2.firstPage();
+  do{
+    u8g2.setCursor(2,14);
+    u8g2.print("系统启动中...");
+  }while(u8g2.nextPage());
 }
 
 void display_update_info(bool mode_auto, bool main_on, uint16_t light_val, uint16_t light_max, 
                          float temp_val, float temp_max, bool led_on, bool fan_on) {
-  u8g2.clearBuffer();
-  
-  u8g2.drawFrame(0, 0, 128, 64);
-  
-  char buf[32];
-  
-  sprintf(buf, "MODE:%s MAIN:%s", mode_auto ? "auto" : "manu", main_on ? "ON" : "OFF");
-  u8g2.drawStr(2, 12, buf);
-  
-  sprintf(buf, "LIGHT:%4d/%4d", light_val, light_max);
-  u8g2.drawStr(2, 28, buf);
-  
-  sprintf(buf, "TEMP:%5.1f/%4.1f", temp_val, temp_max);
-  u8g2.drawStr(2, 44, buf);
-  
-  sprintf(buf, "LED:%s FAN:%s", led_on ? "ON" : "OFF", fan_on ? "ON" : "OFF");
-  u8g2.drawStr(2, 60, buf);
-  
-  u8g2.sendBuffer();
+  u8g2.firstPage();
+  do{
+    u8g2.setCursor(2,14);
+    if(mode_auto)
+      u8g2.print("模式:自动  总闸:");
+    else
+      u8g2.print("模式:手动  总闸:");
+    u8g2.print(main_on?"ON":"OFF");
+
+    u8g2.setCursor(2,28);
+    u8g2.print("光照:");
+    u8g2.print(light_val);
+    u8g2.print("/");
+    u8g2.print(light_max);
+
+    u8g2.setCursor(2,42);
+    u8g2.print("温度:");
+    u8g2.print(temp_val,1);
+    u8g2.print("/");
+    u8g2.print(temp_max,1);
+
+    u8g2.setCursor(2,56);
+    u8g2.print("灯光:");
+    u8g2.print(led_on?"ON":"OFF");
+    u8g2.print(" 风扇:");
+    u8g2.print(fan_on?"ON":"OFF");
+
+  }while(u8g2.nextPage());
 }
